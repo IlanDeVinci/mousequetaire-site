0743e6c25085c8eaa6018d730c878d076d9915ab
IlanDeVinci <ilan.maouchi@gmail.com>
2025-04-15 23:04:22 +0200

    Update page.js

--- app/apropos/page.js
+++ app/apropos/page.js
@@ -473,48 +473,48 @@ export default function Equipe() {
 
           <ScrollReveal animation="fade-right" delay={200} threshold={0.2}>
             <div
-              className="relative px-2 md:px-4 py-20 sm:py-24 md:py-24 flex justify-center overflow-visible w-full cursor-pointer group"
+              className="relative px-2 md:px-4 py-24 sm:py-28 md:py-32 flex justify-center overflow-visible w-full cursor-pointer group"
               onMouseEnter={() => !isMobile && setHoveredValue1(true)}
               onMouseLeave={() => !isMobile && setHoveredValue1(false)}
               onClick={() => isMobile && setHoveredValue1(!hoveredValue1)}
             >
-              {/* Invisible hitbox layer that covers the entire area */}
-              <div className="absolute inset-0 z-40"></div>
+              {/* Invisible hitbox layer that covers the rectangle area */}
+              <div className="absolute top-1/2 left-[40px] md:left-[60px] transform -translate-y-1/2 h-[180px] sm:h-[180px] md:h-[180px] lg:h-[200px] w-[85%] sm:w-[80%] md:w-[65%] lg:w-[55%] z-40"></div>
 
               <div
-                className={`absolute rounded-r-full bg-[#00527A] text-white p-4 md:p-8 
-                        h-[120px] sm:h-[140px] md:h-[160px] lg:h-[180px] left-0 z-10
+                className={`absolute top-0 rounded-r-full bg-[#00527A] text-white p-4 md:p-8 
+                        h-[180px] sm:h-[180px] md:h-[180px] lg:h-[200px] left-0 z-10
                         transition-all duration-300 ease-in-out
                         ${
                           hoveredValue1
-                            ? "w-[98vw] md:w-[85vw] lg:w-[80vw]"
-                            : "w-[70%] sm:w-[70%] md:w-[50%] lg:w-[36%]"
+                            ? "w-[95vw] md:w-[85vw] lg:w-[80vw]"
+                            : "w-[85%] sm:w-[80%] md:w-[60%] lg:w-[45%]"
                         } group-hover:shadow-lg`}
               ></div>
 
               <div
-                className={`absolute rounded-r-full bg-[#006A9E] text-white p-4 md:p-8 
-                        h-[120px] sm:h-[140px] md:h-[160px] lg:h-[180px] left-[-20px] md:left-[-40px] z-20
+                className={`absolute top-0 rounded-r-full bg-[#006A9E] text-white p-4 md:p-8 
+                        h-[180px] sm:h-[180px] md:h-[180px] lg:h-[200px] left-[-20px] md:left-[-40px] z-20
                         transition-all duration-300 ease-in-out
                         ${
                           hoveredValue1
-                            ? "w-[98vw] md:w-[85vw] lg:w-[80vw]"
-                            : "w-[70%] sm:w-[70%] md:w-[50%] lg:w-[36%]"
+                            ? "w-[95vw] md:w-[85vw] lg:w-[80vw]"
+                            : "w-[85%] sm:w-[80%] md:w-[60%] lg:w-[45%]"
                         } group-hover:shadow-lg`}
               ></div>
 
               <div
-                className={`absolute rounded-r-full bg-[#0091D9] text-white pl-4 md:pl-8 
+                className={`absolute top-0 rounded-r-full bg-[#0091D9] text-white pl-4 md:pl-8 
                         flex items-center gap-2 md:gap-4 
                         transition-all duration-300 ease-in-out
-                        h-[120px] sm:h-[140px] md:h-[160px] lg:h-[180px] left-[-40px] md:left-[-80px] z-30
+                        h-[180px] sm:h-[180px] md:h-[180px] lg:h-[200px] left-[-40px] md:left-[-80px] z-30
                         ${
                           hoveredValue1
-                            ? "w-[98vw] md:w-[85vw] lg:w-[80vw]"
-                            : "w-[70%] sm:w-[70%] md:w-[50%] lg:w-[36%]"
+                            ? "w-[95vw] md:w-[85vw] lg:w-[80vw]"
+                            : "w-[85%] sm:w-[80%] md:w-[60%] lg:w-[45%]"
                         } group-hover:shadow-lg`}
               >
-                <div className="shrink-0 ml-[40px] md:ml-[60px] lg:ml-[80px]">
+                <div className="shrink-0 ml-[40px] md:ml-[60px] lg:ml-[80px] -left-8 md:left-0">
                   <Image
                     src="/images/valeur1.svg"
                     alt="Icon Entraide"
@@ -532,7 +532,7 @@ export default function Equipe() {
                     } transition-all duration-300 text-left`}
                   >
                     {hoveredValue1 ? (
-                      <span className="animate-fadeIn overflow-y-auto max-h-[95px] sm:max-h-[120px] md:max-h-[140px] lg:max-h-[160px] block w-[75vw] sm:w-[70vw] md:w-[60vw] font-montserrat font-bold text-[10px] xs:text-[11px] sm:text-xs md:text-sm">
+                      <span className="animate-fadeIn overflow-y-auto max-h-{170px] sm:max-h-[160px] md:max-h-[150px] lg:max-h-[170px] block w-[60vw] sm:w-[50vw] md:w-[55vw] lg:w-[65vw] font-montserrat font-bold text-[10px] xs:text-[10px] sm:text-xs md:text-[13px] lg:text-sm">
                         L&apos;entraide est au cœur de notre ADN, car nous
                         savons que les meilleures solutions naissent du partage
                         des idées et des compétences. Notre collaboration
@@ -553,45 +553,45 @@ export default function Equipe() {
 
           <ScrollReveal animation="fade-left" delay={300} threshold={0.2}>
             <div
-              className="relative px-2 md:px-4 py-20 sm:py-24 md:py-24 flex justify-center overflow-visible w-full cursor-pointer group"
+              className="relative px-2 md:px-4 py-24 sm:py-28 md:py-32 flex justify-center overflow-visible w-full cursor-pointer group"
               onMouseEnter={() => !isMobile && setHoveredValue2(true)}
               onMouseLeave={() => !isMobile && setHoveredValue2(false)}
               onClick={() => isMobile && setHoveredValue2(!hoveredValue2)}
             >
-              {/* Invisible hitbox layer that covers the entire area */}
-              <div className="absolute inset-0 z-40"></div>
+              {/* Invisible hitbox layer that covers the rectangle area */}
+              <div className="absolute top-1/2 right-[40px] md:right-[60px] transform -translate-y-1/2 h-[180px] sm:h-[180px] md:h-[180px] lg:h-[200px] w-[85%] sm:w-[80%] md:w-[65%] lg:w-[55%] z-40"></div>
 
               <div
-                className={`absolute rounded-l-full bg-[#00527A] text-white p-4 md:p-8 
-                        h-[120px] sm:h-[140px] md:h-[160px] lg:h-[180px] right-0 z-10
+                className={`absolute top-0 rounded-l-full bg-[#00527A] text-white p-4 md:p-8 
+                        h-[180px] sm:h-[180px] md:h-[180px] lg:h-[200px] right-0 z-10
                         transition-all duration-300 ease-in-out
                         ${
                           hoveredValue2
-                            ? "w-[98vw] md:w-[85vw] lg:w-[80vw]"
-                            : "w-[70%] sm:w-[70%] md:w-[50%] lg:w-[36%]"
+                            ? "w-[95vw] md:w-[85vw] lg:w-[80vw]"
+                            : "w-[85%] sm:w-[80%] md:w-[60%] lg:w-[45%]"
                         } group-hover:shadow-lg`}
               ></div>
 
               <div
-                className={`absolute rounded-l-full bg-[#006A9E] text-white p-4 md:p-8 
-                        h-[120px] sm:h-[140px] md:h-[160px] lg:h-[180px] right-[-20px] md:right-[-40px] z-20
+                className={`absolute top-0 rounded-l-full bg-[#006A9E] text-white p-4 md:p-8 
+                        h-[180px] sm:h-[180px] md:h-[180px] lg:h-[200px] right-[-20px] md:right-[-40px] z-20
                         transition-all duration-300 ease-in-out
                         ${
                           hoveredValue2
-                            ? "w-[98vw] md:w-[85vw] lg:w-[80vw]"
-                            : "w-[70%] sm:w-[70%] md:w-[50%] lg:w-[36%]"
+                            ? "w-[95vw] md:w-[85vw] lg:w-[80vw]"
+                            : "w-[85%] sm:w-[80%] md:w-[60%] lg:w-[45%]"
                         } group-hover:shadow-lg`}
               ></div>
 
               <div
-                className={`absolute rounded-l-full bg-[#0091D9] text-white pr-4 md:pr-8 
+                className={`absolute top-0 rounded-l-full bg-[#0091D9] text-white pr-4 md:pr-8 
                         flex items-center justify-end gap-2 md:gap-4
                         transition-all duration-300 ease-in-out
-                        h-[120px] sm:h-[140px] md:h-[160px] lg:h-[180px] right-[-40px] md:right-[-80px] z-30
+                        h-[180px] sm:h-[180px] md:h-[180px] lg:h-[200px] right-[-40px] md:right-[-80px] z-30
                         ${
                           hoveredValue2
-                            ? "w-[98vw] md:w-[85vw] lg:w-[80vw]"
-                            : "w-[70%] sm:w-[70%] md:w-[50%] lg:w-[36%]"
+                            ? "w-[95vw] md:w-[85vw] lg:w-[80vw]"
+                            : "w-[85%] sm:w-[80%] md:w-[60%] lg:w-[45%]"
                         } group-hover:shadow-lg`}
               >
                 <div className="overflow-hidden h-full flex items-center grow justify-end">
@@ -603,7 +603,7 @@ export default function Equipe() {
                     } transition-all duration-300 text-right`}
                   >
                     {hoveredValue2 ? (
-                      <span className="animate-fadeIn overflow-y-auto max-h-[95px] sm:max-h-[120px] md:max-h-[140px] lg:max-h-[160px] block w-[75vw] sm:w-[70vw] md:w-[60vw] font-montserrat font-bold text-[10px] xs:text-[11px] sm:text-xs md:text-sm">
+                      <span className="animate-fadeIn overflow-y-auto max-h-{170px] sm:max-h-[160px] md:max-h-[150px] lg:max-h-[170px] block w-[60vw] sm:w-[50vw] md:w-[55vw] lg:w-[65vw] font-montserrat font-bold text-[10px] xs:text-[11px] sm:text-xs md:text-[13px] lg:text-sm">
                         L&apos;innovation guide chacune de nos décisions, nous
                         poussant constamment à explorer de nouvelles approches
                         pour résoudre vos défis numériques. Notre créativité se
@@ -633,45 +633,45 @@ export default function Equipe() {
 
           <ScrollReveal animation="fade-right" delay={400} threshold={0.2}>
             <div
-              className="relative px-2 md:px-4 py-20 sm:py-24 md:py-24 flex justify-center overflow-visible w-full cursor-pointer group"
+              className="relative px-2 md:px-4 py-24 sm:py-28 md:py-32 flex justify-center overflow-visible w-full cursor-pointer group"
               onMouseEnter={() => !isMobile && setHoveredValue3(true)}
               onMouseLeave={() => !isMobile && setHoveredValue3(false)}
               onClick={() => isMobile && setHoveredValue3(!hoveredValue3)}
             >
-              {/* Invisible hitbox layer that covers the entire area */}
-              <div className="absolute inset-0 z-40"></div>
+              {/* Invisible hitbox layer that covers the rectangle area */}
+              <div className="absolute top-1/2 left-[40px] md:left-[60px] transform -translate-y-1/2 h-[180px] sm:h-[180px] md:h-[180px] lg:h-[200px] w-[85%] sm:w-[80%] md:w-[65%] lg:w-[55%] z-40"></div>
 
               <div
-                className={`absolute rounded-r-full bg-[#00527A] text-white p-4 md:p-8 
-                        h-[120px] sm:h-[140px] md:h-[160px] lg:h-[180px] left-0 z-10
+                className={`absolute top-0 rounded-r-full bg-[#00527A] text-white p-4 md:p-8 
+                        h-[180px] sm:h-[180px] md:h-[180px] lg:h-[200px] left-0 z-10
                         transition-all duration-300 ease-in-out
                         ${
                           hoveredValue3
-                            ? "w-[98vw] md:w-[85vw] lg:w-[80vw]"
-                            : "w-[70%] sm:w-[70%] md:w-[50%] lg:w-[36%]"
+                            ? "w-[95vw] md:w-[85vw] lg:w-[80vw]"
+                            : "w-[85%] sm:w-[80%] md:w-[60%] lg:w-[45%]"
                         } group-hover:shadow-lg`}
               ></div>
 
               <div
-                className={`absolute rounded-r-full bg-[#006A9E] text-white p-4 md:p-8 
-                        h-[120px] sm:h-[140px] md:h-[160px] lg:h-[180px] left-[-20px] md:left-[-40px] z-20
+                className={`absolute top-0 rounded-r-full bg-[#006A9E] text-white p-4 md:p-8 
+                        h-[180px] sm:h-[180px] md:h-[180px] lg:h-[200px] left-[-20px] md:left-[-40px] z-20
                         transition-all duration-300 ease-in-out
                         ${
                           hoveredValue3
-                            ? "w-[98vw] md:w-[85vw] lg:w-[80vw]"
-                            : "w-[70%] sm:w-[70%] md:w-[50%] lg:w-[36%]"
+                            ? "w-[95vw] md:w-[85vw] lg:w-[80vw]"
+                            : "w-[85%] sm:w-[80%] md:w-[60%] lg:w-[45%]"
                         } group-hover:shadow-lg`}
               ></div>
 
               <div
-                className={`absolute rounded-r-full bg-[#0091D9] text-white pl-4 md:pl-8 
+                className={`absolute top-0 rounded-r-full bg-[#0091D9] text-white pl-4 md:pl-8 
                         flex items-center gap-2 md:gap-4
                         transition-all duration-300 ease-in-out
-                        h-[120px] sm:h-[140px] md:h-[160px] lg:h-[180px] left-[-40px] md:left-[-80px] z-30
+                        h-[180px] sm:h-[180px] md:h-[180px] lg:h-[200px] left-[-40px] md:left-[-80px] z-30
                         ${
                           hoveredValue3
-                            ? "w-[98vw] md:w-[85vw] lg:w-[80vw]"
-                            : "w-[70%] sm:w-[70%] md:w-[50%] lg:w-[36%]"
+                            ? "w-[95vw] md:w-[85vw] lg:w-[80vw]"
+                            : "w-[85%] sm:w-[80%] md:w-[60%] lg:w-[45%]"
                         } group-hover:shadow-lg`}
               >
                 <div className="shrink-0 ml-[40px] md:ml-[60px] lg:ml-[80px]">
@@ -692,7 +692,7 @@ export default function Equipe() {
                     } transition-all duration-300 text-left`}
                   >
                     {hoveredValue3 ? (
-                      <span className="animate-fadeIn overflow-y-auto max-h-[95px] sm:max-h-[120px] md:max-h-[140px] lg:max-h-[160px] block w-[75vw] sm:w-[70vw] md:w-[60vw] font-montserrat font-bold text-[10px] xs:text-[11px] sm:text-xs md:text-sm">
+                      <span className="animate-fadeIn overflow-y-auto max-h-{170px] sm:max-h-[160px] md:max-h-[150px] lg:max-h-[170px] block w-[60vw] sm:w-[50vw] md:w-[55vw] lg:w-[65vw] font-montserrat font-bold text-[10px] xs:text-[11px] sm:text-xs md:text-[13px] lg:text-sm">
                         L&apos;excellence n&apos;est pas négociable pour nous,
                         chaque ligne de code et chaque pixel sont
                         méticuleusement travaillés pour atteindre les plus hauts
